ca19531cfbfc772541f0afe40652ccc5bc9b0fbf
Santosh Lamichhane <ssrusan@gmail.com>
2023-06-22 09:27:51 +0545

    counted duplicate elements in array

--- JavaScript/Array/level4.js
+++ JavaScript/Array/level4.js
@@ -1,8 +1,24 @@
-//Q1 find if hari is in allUsers array or not/
-//expected output is : 
-//true
+// Q1 find if hari is in allUsers array or not/
+// expected output is : 
+// true
 const name= 'hari'
 const allUsers = ['hari','shyam','thakur']
 
 console.log(allUsers.includes(name)
 )
+
+
+//Q2 find number of duplicates
+//expected output is : 2
+//true
+const user= 'hari'
+const userArr = ['hari','shyam','hari','thakur']
+let count=0
+for(let i=0; i<userArr.length; i++){
+  
+    if(userArr[i]==user){
+        count++;
+    }
+
+}
+console.log(count)
