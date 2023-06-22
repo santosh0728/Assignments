e2ea7cd46e2985ab1cbc6b89b07ca874cc2c88a0
Santosh Lamichhane <ssrusan@gmail.com>
2023-06-22 09:05:24 +0545

    solved jslevel 4

---
+++ JavaScript/Array/level4.js
@@ -0,0 +1,8 @@
+//Q1 find if hari is in allUsers array or not/
+//expected output is : 
+//true
+const name= 'hari'
+const allUsers = ['hari','shyam','thakur']
+
+console.log(allUsers.includes(name)
+)
